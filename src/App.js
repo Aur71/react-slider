d11ef6c700e86b6da0dcfcfa3132e403f3c1e20f
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-10-14 12:05:13 +0300

    Deploy prep

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
 import data from './data';
 import { FaQuoteRight } from 'react-icons/fa';
